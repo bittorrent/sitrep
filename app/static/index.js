78297685b97bc1a7621a92edca4ea3c78733c952
Christopher Brown <cbrown@bittorrent.com>
2016-06-08 22:21:16 -0700

    add text, make a little nicer looking

---
+++ app/static/index.js
@@ -0,0 +1,28 @@
+// jshint esversion:6
+
+function colorForHealth(health, saturation = 1.0, brightness = 0.5) {
+    return 'hsl(' + Math.round(health / 100 * 120) + ', ' + Math.round(saturation * 100) + '%, ' + Math.round(brightness * 100) + '%)';
+}
+
+$(function() {
+    $.get('/api/v1/components', function(data) {
+        $.each(data.components, function(id, component) {
+            var $group = $('#other');
+            if (component.tags.type == 'swarm-server') {
+                $group = $('#swarm-servers');
+            }
+            $group.show();
+
+            var $row = $('<div class="component">');
+            var $header = $('<header>');
+            $header.append($('<span class="label">').text(component.label));
+            $header.append($('<span class="status" style="background-color: ' + colorForHealth(component.health, 0.5, 0.8) + '; color: ' + colorForHealth(component.health, 0.3, 0.4) + '">').text(component.status));
+            $row.append($header);
+            if (component.status_description) {
+                $row.append($('<div class="description">').text(component.status_description));
+            }
+
+            $group.find('.components').append($row);
+        });
+    });
+});
