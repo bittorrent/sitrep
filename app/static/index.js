ec6d7eb1c69c640915bd2e249811e96c398cf939
Loc Dinh <ldinh@bittorrent.com>
2016-09-14 11:15:56 -0700

    new layout for status page to show more charts
    Summary: new layout for status page to show more charts
    
    Test Plan: eye test {F2929}
    
    Reviewers: cbrown
    
    Reviewed By: cbrown
    
    Tags: #web
    
    Differential Revision: https://phabricator.btlive.tv/D1750

--- app/static/index.js
+++ app/static/index.js
@@ -1,60 +1,127 @@
 function colorForHealth(health, saturation, brightness) {
-    return 'hsl(' + Math.round(health / 100 * 120) + ', ' + Math.round(saturation * 100) + '%, ' + Math.round(brightness * 100) + '%)';
+    return 'rgb(' + HSLToRGB((health / 100) * (120 / 360), saturation, brightness).join(',') + ')';
 }
 
-$(function() {
+function hueToRGB(p, q, t){
+    if (t < 0) { t += 1; }
+    if (t > 1) { t -= 1; }
+    if (t < 1/6) { return p + (q - p) * 6 * t; }
+    if (t < 1/2) { return q; }
+    if (t < 2/3) { return p + (q - p) * (2/3 - t) * 6; }
+    return p;
+}
+
+function HSLToRGB(h, s, l) {
+    var r, g, b;
+
+    if (s === 0){
+        r = g = b = l;
+    } else {
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hueToRGB(p, q, h + 1/3);
+        g = hueToRGB(p, q, h);
+        b = hueToRGB(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function init() {
     $.get('/api/v1/components', function(data) {
         $.each(data.components, function(id, component) {
-            var $group = $('#other');
-            if (component.tags.type == 'swarm-server') {
-                $group = $('#swarm-servers');
-            }
-            $group.show();
-
-            var now = Math.floor(Date.now() / 1000);
-
-            var $row = $('<div class="component">');
-            var $header = $('<header>');
-            $header.append($('<span class="label">').text(component.label));
-            $header.append($('<span class="status" style="background-color: ' + colorForHealth(component.health, 0.5, 0.8) + '; color: ' + colorForHealth(component.health, 0.3, 0.4) + '">').text(component.status));
-            $row.append($header);
-
-            if (component.status_description) {
-                $row.append($('<div class="description">').text(component.status_description));
-            }
-
-            var $history = $('<div class="update-history">');
-            var t = now;
-            var historyScale = 12 * 60 * 60;
-            var uptime = 0;
-            $.each(component.update_history, function(i, update) {
-                var time = new Date(update.time).getTime() / 1000;
-                var start = Math.max(time, now - historyScale);
-                var end = Math.min(t, time + update.lifetime);
-                if (start >= end) {
-                    return;
-                }
-                var width = 100 * (end - start) / historyScale;
-                var $update = $('<span class="update">');
-
-                if (update.status_description) {
-                    $update.attr('data-tooltip', update.status_description);
-                }
-
-                $update.css('left', (start - now + historyScale) / historyScale * 100 + '%');
-                $update.css('background-color', colorForHealth(update.health, 0.5, 0.8));
-                $update.css('width', width + '%');
-                $update.css('z-index', 100 - update.health);
-                $history.prepend($update);
-
-                uptime += update.health / 100 * (end - start);
-                t = time;
-            });
-            $row.append($history);
-
-            $header.append($('<span class="uptime">').text('12H: ' + (uptime / historyScale) * 100 + '%'));
-
-            $group.find('.components').append($row);
+            componentData = getComponentData(component);
+            populateComponent(componentData);
         });
+    });
+}
+
+function getComponentData(component) {
+    var updateHistory = component.update_history;
+    var now = Math.floor(Date.now() / 1000);
+    var t = now;
+    var historyScale = 12 * 60 * 60;
+    var timelineData = [];
+    var timelineColors = [];
+    var uptime = 0;
+    for(var i = 0; i < updateHistory.length; i++) {
+        var update = updateHistory[i];
+        var time = new Date(update.time).getTime() / 1000;
+        var start = Math.max(time, now - historyScale);
+        var end = Math.min(t, time + update.lifetime);
+        if (start >= end) {
+            break;
+        }
+
+        var status = update.status_description ? update.status_description : (update.health == 100 ? 'Healthy.' : 'Unhealthy.');
+        var data = [update.label, status, new Date(start * 1000), new Date(end * 1000)];
+
+        timelineData.unshift(data);
+        timelineColors.unshift(colorForHealth(update.health, 0.5, 0.8));
+        uptime += update.health / 100 * (end - start);
+        t = time;
+    }
+    var upTime = ((uptime / historyScale) * 100).toFixed(5);
+
+    return {name: component.label, status: component.status, timelineData: timelineData, timelineColors: timelineColors, upTime: ((uptime / historyScale) * 100).toFixed(6) + '%'};
+}
+
+function populateComponent(data) {
+    var $componentContainer = $($('#component-container-template').html()).appendTo('.components-container');
+    var chart = new google.visualization.Timeline($componentContainer.find('.timeline').get(0));
+    var dataTable = new google.visualization.DataTable();
+
+    dataTable.addColumn({
+        type: 'string',
+        id: 'Role'
+    });
+    dataTable.addColumn({
+        type: 'string',
+        id: 'Name'
+    });
+    dataTable.addColumn({
+        type: 'date',
+        id: 'Start'
+    });
+    dataTable.addColumn({
+        type: 'date',
+        id: 'End'
+    });
+    dataTable.addRows(data.timelineData);
+
+    var options = {
+        timeline: {
+            groupByRowLabel: true,
+            showBarLabels: false,
+            showRowLabels: false
+        },
+        avoidOverlappingGridLines: false,
+        colors: data.timelineColors
+    };
+
+    $componentContainer.find('.name').text(data.name);
+    $componentContainer.find('.up-time').text(data.upTime);
+    $componentContainer.find('.status').text(data.status).addClass(data.status == 'stream error' ? 'error' : '');
+
+    $componentContainer.on('resized', function() {
+        chart.draw(dataTable, options);
+    });
+
+    $componentContainer.trigger('resized');
+}
+
+$(function(){
+    google.charts.load("current", {
+        packages: ["timeline"]
+    });
+    google.charts.setOnLoadCallback(init);
+
+    $(window).resize(function () {
+        if($('.components-container').data('width') != $('.components-container').width() && !$('.components-container').data('resizing')) {
+            $('.components-container').data('resizing', true);
+            $('.components-container').data('width', $('.components-container').width());
+            $('.components-container .component-container').trigger('resized');
+            $('.components-container').data('resizing', false);
+        }
     });
 });
